502895a276dce4ea435ca44243fece3a27fb9553
GEARLESSJOOE <444047220@qq.com>
2024-04-24 16:27:36 +0800

    langchain版本，知识图谱查询

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
 import os.path
 
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain_core.documents import Document
+
 from question_match import GraphQA
 
 from config import *
@@ -11,7 +13,7 @@ from langchain.agents import AgentExecutor, create_react_agent
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains.llm_requests import LLMRequestsChain
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma, FAISS
 from langchain.tools import Tool
 from langchain.memory import ConversationBufferMemory
 from langchain import hub
@@ -52,13 +54,89 @@ class Agent():
 
     # Neo4j数据库知识图谱查询
     def graph_func(self, x, question):
-        # 替换langchain，手写知识图谱查询
-        qa = GraphQA()
-        answer = qa.query(question)
+        # # 方法1：替换langchain，利用正则匹配进行识别
+        # qa = GraphQA()
+        # answer = qa.query(question)
+        #
+        # graph_prompt = PromptTemplate(
+        #     template=GRAPH_SUMMARY_TPL,
+        #     partial_variables={"full_information": answer},
+        #     input_variables=["question"]
+        # )
+        #
+        # chain = LLMChain(
+        #     llm=get_llm_model(),
+        #     prompt=graph_prompt,
+        #     verbose=os.getenv("VERBOSE")
+        # )
+        #
+        # res = chain.invoke({"question": question})['text']
+        # return res
 
+        # 方法2：命名实体识别(langchain实现)
+        response_schemas = [
+            # ResponseSchema(type="list", name="tender_company", description="招标公司实体"),
+            ResponseSchema(type="list", name="winning_company", description="中标公司名称实体"),
+            ResponseSchema(type="list", name="winning_price", description="中标金额实体")
+        ]
+        # 格式化模版
+        format_instructions = structured_output_parser(response_schemas=response_schemas)
+
+        # 设置解析器
+        output_parser = StructuredOutputParser(response_schemas=response_schemas)
+
+        ner_prompt = PromptTemplate(
+            template=NER_PROMPT_TPL,
+            partial_variables={"format_instructions": format_instructions},
+            input_variables=["question"]
+        )
+        chain = LLMChain(
+            llm=get_llm_model(),
+            prompt=ner_prompt,
+            verbose=os.getenv("VERBOSE")
+        )
+        res = chain.invoke({"question": question})["text"]
+        ner_res = output_parser.parse(res)  # 必须按照首'''json尾'''进行查找
+        # print(ner_res)
+
+        # 词槽替换
+        graph_templates = []
+        for template in LANGCHAIN_GRAPH_TEMPLATE:
+            slots_key = template["slots"][0]
+            for value in ner_res[slots_key]:
+                question = replace_token_in_string(template["question"], {slots_key: value})
+                cypher = replace_token_in_string(template["cypher"], {slots_key: value})
+                answer = replace_token_in_string(template["answer"], {slots_key: value})
+                graph_templates.append({"question": question, "cypher": cypher, "answer": answer})
+        # print(graph_templates)
+
+        # 相似度检索
+        graph_documents = [Document(page_content=template["question"], metadata=template) for template in
+                           graph_templates]
+        db = FAISS.from_documents(graph_documents, get_embeddings_model())
+        graph_documents_filter = db.similarity_search_with_relevance_scores(question, k=2)
+        # print(graph_documents_filter)
+
+        # 执行CQL,拿最终结果
+        question_result = []
+        graph = get_connect_neo4j()
+        for item in graph_documents_filter:
+            question = item[0].page_content
+            cypher = item[0].metadata["cypher"]
+            answer = item[0].metadata["answer"]
+            try:
+                result = graph.run(cypher).data()
+                if result[0].values():
+                    final_answer = replace_token_in_string(answer, result[0])
+                    question_result.append(f'问题：{question}，回答：{final_answer}')
+                    # print(question_result)
+            except:
+                pass
+
+        # 将结果交给大模型处理总结
         graph_prompt = PromptTemplate(
             template=GRAPH_SUMMARY_TPL,
-            partial_variables={"full_information": answer},
+            partial_variables={"full_information": question_result},
             input_variables=["question"]
         )
 
@@ -70,35 +148,6 @@ class Agent():
 
         res = chain.invoke({"question": question})['text']
         return res
-        # 命名实体识别(langchain实现)
-        # response_schemas = [
-        #     ResponseSchema(type="list", name="tender_name", description="招标项目实体"),
-        #     ResponseSchema(type="list", name="winning_company", description="中标公司实体"),
-        #     ResponseSchema(type="list", name="winning_price", description="中标金额实体")
-        # ]
-        # # 格式化模版
-        # format_instructions = structured_output_parser(response_schemas=response_schemas)
-        #
-        # # 设置解析器
-        # output_parser = StructuredOutputParser(response_schemas=response_schemas)
-        #
-        # ner_prompt = PromptTemplate(
-        #     template=NER_PROMPT_TPL,
-        #     partial_variables={"format_instructions": format_instructions},
-        #     input_variables=["question"]
-        # )
-        # chain = LLMChain(
-        #     llm=get_llm_model(),
-        #     prompt=ner_prompt,
-        #     verbose=os.getenv("VERBOSE")
-        # )
-        # res = chain.invoke({"question": question})["text"]
-        # # print(res)
-        # # exit()
-        # ner_res = output_parser.parse(res)  # 必须按照首'''json尾'''进行查找
-        # # print(ner_res)
-        # # exit()
-        # return ner_res
 
     # 利用360进行搜索查询
     def search_func(self, question):
@@ -210,5 +259,6 @@ if __name__ == '__main__':
     # print(agent.graph_func("中南建筑设计院股份有限公司赢得了华中农业大学学生宿舍及生活配套设施项目方案设计华中农业大学学生宿舍及生活配套设施项目方案，价格为198989"))
     # agent.graph_func("zhangjunfeng")
     # agent.query("武汉斯泰德建设科技有限公司, 武汉江腾铁路工程有限责任公司得到了哪些公司的项目？")
-    agent.query("武汉天河机场有限责任公司招标项目都有哪些？")
-    # print(agent.graph_func("武汉斯泰德建设科技有限公司, 武汉江腾铁路工程有限责任公司得到了哪些公司的项目？"))
+    agent.query("中南建筑设计院股份有限公司招标项目都有哪些？")
+    # print(agent.graph_func(x=[], question="中南建筑设计院股份有限公司，以及武汉斯泰德建设科技有限公司都有什么项目"))
+    # print(agent.graph_func(x=[], question="如果我有8000，我能招标什么项目"))
--- config.py
+++ config.py
@@ -5,9 +5,9 @@ CRAWLER_PATH = os.path.join(BASE_PATH, "data/crawler/tender_data.json")
 TENDER_ENTITY_PATH = os.path.join(BASE_PATH, "data/entity/tender_entity")
 WINNER_ENTITY_PATH = os.path.join(BASE_PATH, "data/entity/winner_entity")
 
-GRAPH_LINK = ""
-AUTH_USERNAME = ""
-AUTH_PASSWORD = ""
+GRAPH_LINK = "bolt://localhost:7687"
+AUTH_USERNAME = "neo4j"
+AUTH_PASSWORD= "zjf568938593"
 THRESHOLD = 0.1
 
 
@@ -36,3 +36,22 @@ TEMPLATES = [
         'example': '武汉天河机场有限责任公司都有哪些招标项目？'
     },
 ]
+
+LANGCHAIN_GRAPH_TEMPLATE = [
+    {
+        'question': '%winning_company%有什么项目？',
+        'cypher': "match (n1 {winning_company:'%winning_company%'})-[:Get]->(n2) return substring(reduce(s = '', x in collect(n2.tender_name) | s + '/' + x), 1) as res",
+        'answer': '%winning_company%的项目有：%res%',
+        'slots': ["winning_company"],
+        'example': '中建三局集团有限公司有什么项目？'
+    },
+    {
+        'question': '如果我有%winning_price%元，可以获得哪些招标项目？',
+        'cypher': "match(n:Winner) where n.winning_price < tointeger(%winning_price%) match (n)-[:Get]->(m) return substring(reduce(s = '', x in collect(m.tender_name) | s + '/' + x), 1) as res",
+        'answer': '如果有%winning_price%元可以获得的招标项目有：%res%',
+        'slots': ["winning_price"],
+        'example': '如果我有4000元，可以获得哪些招标项目？'
+    }
+]
+
+
